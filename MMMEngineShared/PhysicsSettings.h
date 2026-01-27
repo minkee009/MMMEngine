9e2140f72b1700b47077f4d459acd11af00c69d7
minkee009 <minkee5004@gmail.com>
2026-01-27 11:30:03 +0900

    유저스크립트 예제 강화

---
+++ MMMEngineShared/PhysicsSettings.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "ExportSingleton.hpp"
+#include <filesystem>
+
+namespace MMMEngine
+{
+	class PhysicsSettings : public Utility::ExportSingleton<PhysicsSettings>
+	{
+	public:
+		void SaveSettings(std::filesystem::path filePath);
+		void LoadSettings(std::filesystem::path filePath);
+	private:
+		std::filesystem::path m_settingsPath;
+	};
+}
