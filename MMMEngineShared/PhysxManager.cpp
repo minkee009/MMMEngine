0d6f1deb109351d2801ca889aff6b64dab120145
Kimjunghyeon <130948037+racoonisdog@users.noreply.github.com>
2026-02-05 10:21:45 +0900

    fixed - 함수 재정의 오류 수정

--- MMMEngineShared/PhysxManager.cpp
+++ MMMEngineShared/PhysxManager.cpp
@@ -403,9 +403,9 @@ void MMMEngine::PhysxManager::CollectCollidersInSubtree(ObjPtr<GameObject> root,
 bool MMMEngine::PhysxManager::SweepSphere(const Vector3& center, float radius,
     const Vector3& dir, float maxDist,
     SweepHit& out, uint32_t layer,
-    ObjPtr<ColliderComponent> ignoreCol = nullptr,
-    ObjPtr<RigidBodyComponent> ignoreRb = nullptr,
-    bool includeTrigger = false)
+    ObjPtr<ColliderComponent> ignoreCol,
+    ObjPtr<RigidBodyComponent> ignoreRb,
+    bool includeTrigger)
 {
     physx::PxSphereGeometry geom(radius);
     physx::PxTransform pose(ToPxVec(center));
